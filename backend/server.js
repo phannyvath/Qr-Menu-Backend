d4caf3bf45803092f91523724a732711f9904084
phannyvath <113624373+phannyvath@users.noreply.github.com>
2025-05-28 00:54:51 +0700

    edit

--- backend/server.js
+++ backend/server.js
@@ -50,7 +50,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/foods", require("./routes/foodRoutes"));
 app.use("/api/features", require("./routes/featureRoutes"));
-app.use("/api/orders", require("./routes/orderRoutes")); // Your order routes
+app.use("/api", require("./routes/orderRoutes")); // Your order routes
 app.use("/api/payment", require("./routes/paymentRoutes")); // New payment routes
 app.use("/api/categories", require("./routes/categoryRoutes")); // ✅ Add this line
 app.use("/api/tables", require("./routes/tableRoutes"));
